03691cf63312c08fcb3e925c2b8b523ebb2fcde6
Adejoke <adejokeogunyinka@enyata.com>
2024-05-10 15:30:24 +0100

    fix: changed component file names

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { Input } from "./CowryInput";
 export { Button } from "./CowryButton";
-export { TextArea } from "./TextArea";
+export { TextArea } from "./CowryTextArea";
 export { DashboardMenu } from "./CowryDashboardMenu";
 export { SignUp } from "./forms/SignUp";
 export { Login } from "./forms/Login";
