3c2650f104b2cf478a9594aad6760b7dad0f2dd5
Adejoke <adejokeogunyinka@enyata.com>
2024-05-13 13:15:58 +0100

    fix: API url

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,4 @@
 export const AVATAR_API = "https://ui-avatars.com/api";
-export const API = "https://brt-online-ticket-booking-backend.onrender.com/";
+export const API = "http://localhost:1337/api";
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
